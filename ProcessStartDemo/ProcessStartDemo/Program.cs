97eeb690c3b378018e8401deb51ba077302106ae
ncepuhxh <ncepuhxh@qq.com>
2017-03-03 19:46:19 +0800

    this demo of test start a process by code

---
+++ ProcessStartDemo/ProcessStartDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessStartDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appName = "QQ";//the path of the exe file
+            string appPath = @"C:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe";//the path of the exe file
+            bool runFlag = false;
+
+            Process[] myProcesses = Process.GetProcesses();
+            foreach (Process myProcess in myProcesses)
+            {
+                if (myProcess.ProcessName.CompareTo(appName) == 0)
+                {
+                    runFlag = true;
+                }
+            }
+
+            if (!runFlag)   //如果程序没有启动
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = appName;
+                proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(appPath);
+                proc.Start();
+
+
+            }
+            else if (runFlag)   //如果程序已经启动
+            {
+                Process[] myPro = Process.GetProcessesByName(appName);
+                myPro[0].Kill();       //删除进程       
+
+                Process proc = new Process();
+                proc.StartInfo.FileName = appName;
+                proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(appPath);
+                proc.Start();
+
+            }
+        }
+    }
+}
